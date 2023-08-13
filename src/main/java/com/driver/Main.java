8957d8d8206785ade9fb175e0203fda6c536cbe3
Nilesh Bahetwar <bahetwarnilesh2310@gmail.com>
2023-08-13 14:29:43 +0530

    polymorphismAss

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public static int product(int x,int y){
+            return x*y;
+        }
+        public static int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public static double product(double x,double y){
+            return x*y;
+        }
+    }
+      public static void main(String[] args){
+          Product p = new Product();
+          p.product(22,33);
+          p.product(11,22,33);
+          p.product(11.22,33.44);
+      }
 }
